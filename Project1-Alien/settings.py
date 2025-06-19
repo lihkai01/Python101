7e68672c29566ad52bd26629b984b7ee3c3b921c
lihkai01 <lihkai0112@gmail.com>
2025-06-19 22:21:56 +0800

    1906 - Alien 1 commit

---
+++ Project1-Alien/settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A class to store all setting for Alien Invasion."""
+
+    def __init__(self):
+        # Screen Settings
+        self.screen_width  = 1200
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
+
+        # Ship settings
+        self.ship_speed = 1.5
