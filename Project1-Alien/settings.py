fa15d2da7021c4c7e366dcad5c92e4ad6853d412
lihkai01 <lihkai0112@gmail.com>
2025-06-20 11:02:00 +0800

    2006- Bullet update

--- Project1-Alien/settings.py
+++ Project1-Alien/settings.py
@@ -9,3 +9,10 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 5
+        self.bullet_color = (60,60,60)
+        self.bullet_allowed = 3
